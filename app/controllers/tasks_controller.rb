83ee11730c1dae5dc99080108e8519a312213645
Jesus Marin <jesusangelm@gmail.com>
2015-05-07 10:25:53 -0430

    add actions in controller for create and show tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,28 @@
 class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
+  before_action :set_tasks, only: [:edit, :update, :destroy]
   respond_to :html, :js
 
-  def index
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+  private
+
+  def set_tasks
+    @task.find(params[:id])
+  end
+
+  def all_tasks
     @tasks = Task.order(created_at: :desc)
+  end
+
+  def task_params
+    params.require(:task).permit(:content, :complete)
   end
 
 end
