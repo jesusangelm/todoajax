175d80192c73722e60f5bcd2b0f95212d0d1131d
Jesus Marin <jesusangelm@gmail.com>
2015-05-20 15:57:11 -0430

    ajust Task CRUD actions for current user authenticated.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,15 @@
 class TasksController < ApplicationController
+  before_filter :authenticate_user!
   before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_tasks, only: [:edit, :update, :destroy]
   respond_to :html, :js
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
 
   def create
-    @task = Task.create(task_params)
+    @task = current_user.tasks.create(task_params)
   end
 
   def update
@@ -22,11 +23,11 @@ class TasksController < ApplicationController
   private
 
   def set_tasks
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def all_tasks
-    @tasks = Task.order(created_at: :desc)
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def task_params
