55961881be151fa338e11da3f729624eda84624f
Jesus Marin <jesusangelm@gmail.com>
2015-06-01 11:57:44 -0430

    send devise emails using background jobs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tasks, dependent: :destroy
 
@@ -10,5 +10,9 @@ class User < ActiveRecord::Base
 
   def send_email_notification
     SendEmailJob.new.async.perform(self)
+  end
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
   end
 end
